ca539ee159ee620eeb9d2fc2d83540aee9a2bb99
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-07-18 18:55:55 +0000

    Create set.js

---
+++ set.js
@@ -0,0 +1,6 @@
+const fs = require('fs-extra');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+module.exports = { session: process.env.SESSION_ID || 'zokk',
+    PREFIXE: process.env.PREFIXE
+};
