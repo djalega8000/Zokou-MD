097f43fdcc5ae5d5084bfab19ff3e6033daba0c3
Faouz995 <105295384+Faouz995@users.noreply.github.com>
2023-07-26 17:39:06 +0000

    Update converter.js

--- commandes/converter.js
+++ commandes/converter.js
@@ -3,7 +3,7 @@ const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter")
 
 
 ////////////////////////////////////////            /sticker/           ////////////////////////////////////////////
-zokou({ nomCom: "sticker",
+zokou({ nomCom: ["sticker","s"],
     categorie: "converter", reaction: "👨🏿‍💻" }, async (origineMessage, zk, commandeOptions) => {
     const { prefixe, arg, ms, repondre } = commandeOptions;
    if (!ms.quoted) return repondre("Veillez mentionner une image ou une video s'il vous plait")
@@ -25,4 +25,31 @@ zokou({ nomCom: "sticker",
   const stikk = await sticker.toBuffer();
    await zk.sendMessage(dest, stikk, { quoted: infoMessage })
 }
+
+
+////////////////////////////////////////            /stickercrop/           ////////////////////////////////////////////
+zokou({ nomCom: ["stickercrop","scrop"],
+    categorie: "convertion", reaction: "👨🏿‍💻" }, async (origineMessage, zk, commandeOptions) => {
+    const { prefixe, arg, ms, repondre } = commandeOptions;
+   if (!ms.quoted) return repondre("Veillez mentionner une image ou une video s'il vous plait")
+  let image = await ms.quoted.download();
+                repondre("*En cours de traitement*");
+
+  let sticker = new Sticker(image, {
+    pack: 'je sais pas quoi mettre', // The pack name
+    author: 'Zokou-MD', // The author name
+    type: StickerTypes.CROP, // The sticker type
+    categories: ['🤩', '🎉'], // The sticker category
+    id: '12345', // The sticker id
+    quality: 50, // The quality of the output file
+    background: '#000000' // The sticker background color (only for full stickers)
+
+   
+    
+})
+  const stikk = await sticker.toBuffer();
+   await zk.sendMessage(dest, stikk, { quoted: infoMessage })
+                 }
+
+    ////////////////////////////////////////////////// toimg ///////////////////////////////////
 
