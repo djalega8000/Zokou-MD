b58033d02f3ac198831732e86d43728fb3b78f04
Faouz995 <105295384+Faouz995@users.noreply.github.com>
2023-08-07 17:49:06 +0000

    ajouts de apilolhumain

--- set.js
+++ set.js
@@ -4,5 +4,6 @@ if (fs.existsSync('set.env'))
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIXE,
     NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md"
+    API_LOL_HUMAIN: process.env.API_LOL_HUMAIN
 };
 
