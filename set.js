69a378f0dbdb956f16ff62411ca539ddfa4467c4
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-08-09 08:05:11 +0000

    Update set.js

--- set.js
+++ set.js
@@ -3,7 +3,7 @@ if (fs.existsSync('set.env'))
     require('dotenv').config({ path: __dirname + '/set.env' });
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIXE,
-    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md"
+    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
     API_LOL_HUMAIN: process.env.API_LOL_HUMAIN
 };
 
