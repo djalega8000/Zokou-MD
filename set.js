0a99364098048da5b35ce29eba8fe1b2fc231d78
djalega8000 <139915759+djalega8000@users.noreply.github.com>
2023-08-09 22:09:49 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,9 +1,32 @@
 const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
 if (fs.existsSync('set.env'))
     require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIXE,
     NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
-    API_LOL_HUMAIN: process.env.API_LOL_HUMAIN
+  API_LOL_HUMAIN: process.env.API_LOL_HUMAIN   ,MODE: process.env.MODE_PUBLIC,
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+    /* new Sequelize({
+     dialect: 'sqlite',
+     storage: DATABASE_URL,
+     logging: false,
+})
+: new Sequelize(DATABASE_URL, {
+     dialect: 'postgres',
+     ssl: true,
+     protocol: 'postgres',
+     dialectOptions: {
+         native: true,
+         ssl: { require: true, rejectUnauthorized: false },
+     },
+     logging: false,
+}),*/
 };
-
