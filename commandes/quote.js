5def06561d36bff0ac8e6dd1acd08a3a086d6c80
Faouz995 <105295384+Faouz995@users.noreply.github.com>
2023-08-12 11:37:56 +0000

    Update quote.js

--- commandes/quote.js
+++ commandes/quote.js
@@ -1,13 +1,15 @@
 const {zokou} = require ('../framework/zokou')
 
 
-zokou({ nomCom: "citation", categorie: "fun" }, async (dest, zk, commandeOptions) => {
-  const { ms, repondre, verifGroupe } = commandeOptions;
+zokou({ nomCom: "citation", categorie: "Fun" }, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, verifGroupe ,arg } = commandeOptions;
   if (!verifGroupe) {
     repondre("commande réservée au groupe uniquement");
     return;
   }
-
+   if (!arg[0]) {
+     
+  
   try {
     fetch("https://animechan.xyz/api/random")
       .then((response) => response.json())
@@ -19,8 +21,27 @@ zokou({ nomCom: "citation", categorie: "fun" }, async (dest, zk, commandeOptions
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-᚛M๏𝓷keℽ D Lบffy᚜ powered by`));
+propulser par Zokou-MD`));
   } catch (e) {
-    repondre("erreur lors de la génération de la citation " + e);
+  
+    repondre("erreur lors de la génération de la citation " + e);  }
+  } else {
+   const query = arg.join(" ")
+
+     try {
+    fetch("https://animechan.xyz/api/random/character?name="+ query)
+      .then((response) => response.json())
+      .then((quote) => repondre(`╔══════════════════════════╗
+║   Zokou-md               ║
+╚══════════════════════════╝
+
+🎬 Anime: ${quote.anime}
+👤 Character: ${quote.character}
+💬 Quote: ${quote.quote}
+
+propulser par Zokou-MD`));
+  } catch (e) {
+  
+    repondre("erreur lors de la génération de la citation " + e);  }
   }
 });
