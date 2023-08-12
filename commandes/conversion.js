a76cf686b7833545ac77e370fd3a62507223e054
Faouz995 <105295384+Faouz995@users.noreply.github.com>
2023-08-12 11:22:08 +0000

    Update conversion.js

--- commandes/conversion.js
+++ commandes/conversion.js
@@ -5,7 +5,7 @@ let fs=require("fs-extra")
 
 
 
-zokou({nomCom:"sticker",categorie: "conversion", reaction: "👨🏿‍💻"},async(origineMessage,zk,commandeOptions)=>{
+zokou({nomCom:"sticker",categorie: "Conversion", reaction: "👨🏿‍💻"},async(origineMessage,zk,commandeOptions)=>{
 
 let {ms,mtype,arg,repondre,nomAuteurMessage}=commandeOptions
   var txt=JSON.stringify(ms.message)
@@ -69,6 +69,94 @@ const alea = (ext) => {
         arg.includes("-r") || arg.includes("-c")
           ? StickerTypes.CROPPED
           : StickerTypes.FULL,
+      quality: 40,
+    });
+  } else {
+    repondre("Veuillez mentionné une image ou une vidéo!");
+    return;
+  }
+
+  await sticker.toFile(stickerFileName);
+  await zk.sendMessage(
+    origineMessage,
+    {
+      sticker: fs.readFileSync(stickerFileName),
+    },
+    { quoted: ms }
+  );
+
+try{
+  fs.unlinkSync(stickerFileName)
+}catch(e){console.log(e)}
+
+
+
+
+
+  
+});
+
+zokou({nomCom:"stickercrop",categorie: "Conversion", reaction: "👨🏿‍💻"},async(origineMessage,zk,commandeOptions)=>{
+
+let {ms,mtype,arg,repondre,nomAuteurMessage}=commandeOptions
+  var txt=JSON.stringify(ms.message)
+
+  var mime=mtype === "imageMessage" || mtype === "videoMessage";
+  var tagImage = mtype==="extendedTextMessage" && txt.includes("imageMessage")
+  var tagVideo = mtype==="extendedTextMessage" && txt.includes("videoMessage")
+
+const alea = (ext) => {
+  return `${Math.floor(Math.random() * 10000)}${ext}`;};
+
+
+  const stickerFileName = alea(".webp");
+
+
+            // image
+  if (mtype === "imageMessage" ||tagImage) {
+    let downloadFilePath;
+    if (ms.message.imageMessage) {
+      downloadFilePath = ms.message.imageMessage;
+    } else {
+      // image mentionnée
+      downloadFilePath =
+        ms.message.extendedTextMessage.contextInfo.quotedMessage.imageMessage;
+    }
+    // images
+    const media = await downloadContentFromMessage(downloadFilePath, "image");
+    let buffer = Buffer.from([]);
+    for await (const elm of media) {
+      buffer = Buffer.concat([buffer, elm]);
+    }
+
+    sticker = new Sticker(buffer, {
+      pack:"Zokou-Md" ,
+      author: nomAuteurMessage,
+      type:
+        
+          StickerTypes.CROPPED,
+      quality: 100,
+    });
+  } else if (mtype === "videoMessage" || tagVideo) {
+    // videos
+    let downloadFilePath;
+    if (ms.message.videoMessage) {
+      downloadFilePath = ms.message.videoMessage;
+    } else {
+      downloadFilePath =
+        ms.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage;
+    }
+    const stream = await downloadContentFromMessage(downloadFilePath, "video");
+    let buffer = Buffer.from([]);
+    for await (const elm of stream) {
+      buffer = Buffer.concat([buffer, elm]);
+    }
+
+    sticker = new Sticker(buffer, {
+      pack:"Zokou-Md", // pack stick
+      author:  nomAuteurMessage, // nom de l auteur du stick
+      type:
+      StickerTypes.CROPPED,
       quality: 40,
     });
   } else {
